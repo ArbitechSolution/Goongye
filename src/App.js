244651f0540c6021ee6fcd2d178617b42938da52
ArbitechSolution <93334298+ZahidRiaz01@users.noreply.github.com>
2022-04-15 15:24:37 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/Navbar/navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Staking from "./Components/Staking/Staking";
 
 function App() {
